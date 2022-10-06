2dcf93dc126dfd81381ef8155f3263470bdeee55
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-10-06 15:39:19 +0500

    fix

---
+++ precache-manifest.8e4760fe05db817705b27a4a7537a423.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70488914f4d6704cb4b8",
+    "url": "/css/app.5085fd83.css"
+  },
+  {
+    "revision": "a0f473577f2d3fd4d83a",
+    "url": "/css/chunk-vendors.f58a2721.css"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/fonts/MontserratBold.88932dad.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/fonts/MontserratLight.100b38fa.ttf"
+  },
+  {
+    "revision": "2b4a206bd1b84a6ba751ad7c231d2041",
+    "url": "/img/fon.2b4a206b.png"
+  },
+  {
+    "revision": "c15217081f5999092eacef6933f9c693",
+    "url": "/img/logoor4.c1521708.png"
+  },
+  {
+    "revision": "77ffe08d5f1540b3a34ca552409b86ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260547c40c2b7d8c1af5",
+    "url": "/js/about.b2f9a6d9.js"
+  },
+  {
+    "revision": "70488914f4d6704cb4b8",
+    "url": "/js/app.2eabd25a.js"
+  },
+  {
+    "revision": "a0f473577f2d3fd4d83a",
+    "url": "/js/chunk-vendors.3f15c636.js"
+  },
+  {
+    "revision": "4d0132f4513c5cfc4d24e790841ba6e6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
